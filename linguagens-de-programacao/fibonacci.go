4557a94d06c10f99ab23fd179f77cd049bfe58ca
Fernando Porazzi <fporazzi46@gmail.com>
2015-03-12 17:06:01 -0300

    Apply coding styles to GO code

--- linguagens-de-programacao/fibonacci.go
+++ linguagens-de-programacao/fibonacci.go
@@ -5,53 +5,53 @@
 package main
 
 import(
-        "fmt"
+    "fmt"
 )
 
 func isValid(n int) bool {
-        if n == 1 || n == 2 {
-                return false
-        } else {
-                return true
-        }
+    if n == 1 || n == 2 {
+        return false
+    } else {
+        return true
+    }
 }
 
 func fibIterative(n int) int {
-        if x := isValid(n); x == false {
-                return 1
-        } else {
-                j, k, t := 1, 0, 0
+    if x := isValid(n); x == false {
+        return 1
+    } else {
+        j, k, t := 1, 0, 0
 
-                for i := 1; i < n; i++ {
-                        t = j
-                        j = j + k
-                        k = t
-                }
-
-                return j
+        for i := 1; i < n; i++ {
+            t = j
+            j = j + k
+            k = t
         }
+
+        return j
+    }
 }
 
 func fibRecursive(n int) int {
-        if x := isValid(n); x == false {
-                return 1
-        } else {
-                return fibRecursive(n - 1) + fibRecursive(n - 2)
-        }
+    if x := isValid(n); x == false {
+        return 1
+    } else {
+        return fibRecursive(n - 1) + fibRecursive(n - 2)
+    }
 }
 
 func main() {
-        var n int
+    var n int
 
-        fmt.Printf("Welcome to this f*cking cool Fibonacci program!")
-        fmt.Printf("\n\n")
-        fmt.Printf("Type a number and we will give you the Fibonacci value to that number: ")
+    fmt.Printf("Welcome to this f*cking cool Fibonacci program!")
+    fmt.Printf("\n\n")
+    fmt.Printf("Type a number and we will give you the Fibonacci value to that number: ")
 
-        fmt.Scanf("%d", &n)
+    fmt.Scanf("%d", &n)
 
-        fmt.Printf("\n\n")
-        fmt.Printf("Iterative returned: %d", fibIterative(n))
-        fmt.Printf("\n\n")
-        fmt.Printf("Recursive returned: %d", fibRecursive(n))
-        fmt.Printf("\n\n\n")
+    fmt.Printf("\n\n")
+    fmt.Printf("Iterative returned: %d", fibIterative(n))
+    fmt.Printf("\n\n")
+    fmt.Printf("Recursive returned: %d", fibRecursive(n))
+    fmt.Printf("\n\n\n")
 }
